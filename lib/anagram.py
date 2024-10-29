71fe0886eca2141bf13c8fbd12b56200325fbd9a
+ <+>
2024-10-29 18:33:47 -0400

    Passes Detector Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        sorted_given_word = "".join(sorted(self.word))
+        
+        return [
+            word for word in word_list if sorted_given_word == "".join(sorted(word))
+        ]
+
+hello = Anagram("HELLO")
+hello.match(["ohlel", "cat", "test"])
